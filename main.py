d466d97de62bf67295496a114863df26683b30ca
JuiceSok <juice23071992@gmail.com>
2024-07-18 20:48:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 my_string = input('введите ваше имя: ')
+if my_string .count(','):
+ print('Количество знаков:', my_string .count(','))
+else:
+ print('Символов не найдено')
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(" ",""))
 print(my_string[0])
 print(my_string[-1])
-
-
-
